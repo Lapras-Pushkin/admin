75908c7873685c5d6f825af0cf184b7753e31907
Lapras-Pushkin <15216033126@139.com>
2026-03-22 22:24:33 +0800

    Add files via upload

---
+++ head.h
@@ -0,0 +1,42 @@
+#ifndef mysfuck
+#define mysfuck
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct drug {}drug;
+typedef struct drugdode {}drugdode;
+drugdode* druglist = NULL;
+typedef struct doctor {}doctor;
+typedef struct doctornode {}doctornode;
+doctornode* doctorlist = NULL;
+typedef struct schedule {}schedule;
+schedule* schedulelist = NULL;
+void freedrugList() {};
+void freeDoctorList() {};
+void freeScheduleList() {};
+void loadDrugsFromFile() {};
+void saveDrugsToFile() {};
+void loadDoctorsFromFile() {};
+void saveDoctorsToFile() {};
+void loadSchedulesFromFile() {};
+void saveSchedulesToFile() {};
+void displayAllDrugs() {};
+void addDrug() {};
+void deleteDrug() {};
+void updateDrug() {};
+void queryDrug() {};
+void drugIn() {};
+void drugOut() {};
+void drugMenu() {};
+void displayAllDoctors() {};
+void addDoctor() {};
+void deleteDoctor() {};
+void updateDoctor() {};
+void queryDoctor() {};
+void doctorMenu() {};
+void displaySchedule() {};
+void addSchedule() {};
+void deleteSchedule() {};
+void updateSchedule() {};
+void scheduleMenu() {};
+#endif /* mysfuck */
